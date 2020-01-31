685a8a01462e1a84dc8f8f48521500f00649ba0b
314-Noah <60508844+314-Noah@users.noreply.github.com>
2020-01-31 19:33:30 +0100

    Update script.js

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,12 +1,12 @@
 
 function generate(path) {
 	var url = "https://my-password-manager.herokuapp.com/operations/" + path + "/"
-				+ document.getElementById('name').	value.replace("/", "@@@") 	+ "/"
-				+ document.getElementById('host').	value.replace("/", "@@@") 	+ "/"
-				+ document.getElementById('user').	value.replace("/", "@@@") 	+ "/"
-				+ document.getElementById('pass').	value.replace("/", "@@@") 	+ "/"
-				+ document.getElementById('topic').	value.replace("/", "@@@") 	+ "/"
-				+ document.getElementById('ID').innerHTML.replace("/", "@@@") 	+ "/"
-				+ document.getElementById('color').style.backgroundColor 		+ "/";
+				+ encodeURIComponent(document.getElementById('name').	value.replace("/", "@@@"))	+ "/"
+				+ encodeURIComponent(document.getElementById('host').	value.replace("/", "@@@"))	+ "/"
+				+ encodeURIComponent(document.getElementById('user').	value.replace("/", "@@@")) 	+ "/"
+				+ encodeURIComponent(document.getElementById('pass').	value.replace("/", "@@@"))	+ "/"
+				+ encodeURIComponent(document.getElementById('topic').	value.replace("/", "@@@")) 	+ "/"
+				+ encodeURIComponent(document.getElementById('ID').innerHTML.replace("/", "@@@")) 	+ "/"
+				+ encodeURIComponent(document.getElementById('color').style.backgroundColor)		+ "/";
 	location = url;
 }
